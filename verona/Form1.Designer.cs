358687948fa87c7aa3029af62f0ba4de741ca1ab
Davihhhh <davidelin2005@gmail.com>
2022-10-03 08:58:35 +0200

    3/10/22 risolto ricerca

--- verona/Form1.Designer.cs
+++ verona/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cerca = new System.Windows.Forms.Button();
+            this.inserisci = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.textBox1.Location = new System.Drawing.Point(145, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // cerca
+            // 
+            this.cerca.Location = new System.Drawing.Point(236, 182);
+            this.cerca.Name = "cerca";
+            this.cerca.Size = new System.Drawing.Size(75, 23);
+            this.cerca.TabIndex = 1;
+            this.cerca.Text = "Cerca";
+            this.cerca.UseVisualStyleBackColor = true;
+            this.cerca.Click += new System.EventHandler(this.cerca_Click);
+            // 
+            // inserisci
+            // 
+            this.inserisci.Enabled = false;
+            this.inserisci.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.inserisci.Location = new System.Drawing.Point(183, 75);
+            this.inserisci.Name = "inserisci";
+            this.inserisci.Size = new System.Drawing.Size(186, 31);
+            this.inserisci.TabIndex = 2;
+            this.inserisci.Text = "Insierisci il comune";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(583, 288);
+            this.Controls.Add(this.inserisci);
+            this.Controls.Add(this.cerca);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button cerca;
+        private System.Windows.Forms.TextBox inserisci;
     }
 }
 
--- verona/Form1.cs
+++ verona/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace verona
 {
@@ -16,5 +17,64 @@ namespace verona
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.ControlBox = false;
+        }
+
+        const string filename = @"./veneto_verona.csv";
+        string line = "";
+        int recordLenght = 528;
+        int numerorecord = 2592;
+        char semicolon = ';';
+
+        public void letturafile(string nome)
+        {
+            var sr = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            byte[] br = new byte[recordLenght];
+            BinaryReader reader = new BinaryReader(sr);
+            
+            sr.Seek(0, SeekOrigin.Begin);
+            byte[] bt = Encoding.ASCII.GetBytes(nome);
+
+            int i = 0;
+            int j = numerorecord;
+            int m = 0;
+            int pos = -1;
+            int chg = 0;
+
+            do
+            {
+                m = (i + j) / 2;
+                chg = m * recordLenght;
+                sr.Seek(chg, SeekOrigin.Begin);
+                byte[] line = reader.ReadBytes(recordLenght);
+                string linestr = Encoding.ASCII.GetString(line);
+                int index = linestr.IndexOf(semicolon);
+
+                byte[] bnt = reader.ReadBytes(index);
+                if (bt == bnt)
+                    pos = m;
+                else if (bt[0] < bnt[0])
+                    i = m + 1;
+                else
+                    j = m - 1;
+            } while (i <= j && pos == -1);
+
+            MessageBox.Show(sr.Seek(pos, SeekOrigin.Begin).ToString());
+        }
+
+        private void cerca_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            bool check = string.IsNullOrEmpty(nome);
+            if (check)
+            {
+                MessageBox.Show("inserisci il comune da cercare!");
+            }
+            else
+            letturafile(nome);
+        }         
     }
 }
